4ef4824da3d32868346769b184dd355ad4ff6eb8
natandovale <natandovale496@gmail.com>
2021-10-09 14:44:27 -0300

    Laco de repeticao foreach

---
+++ EstruturaDeRepeticao/Foreach/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] diasSemanas = new string[] { "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab" };
+
+            foreach (var item in diasSemanas)
+            {
+                WriteLine($"Dia => {item}");
+            }
+        }
+    }
+}
