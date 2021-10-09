d5531d22765a9e274879f5b6038693f2dd5048fc
natandovale <natandovale496@gmail.com>
2021-10-09 14:36:48 -0300

    Laco de repeticao for

---
+++ EstruturaDeRepeticao/For/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[] { "Natan", "Bruce Wayne" };
+
+            for(int i = 0; i < nomes.Length; i++)
+            {
+                WriteLine($"Olá {nomes[i]}");
+            }
+        }
+    }
+}
