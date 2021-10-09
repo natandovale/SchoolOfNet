e2ebb59588ad91bbb2c7a5418595f053e22fe637
natandovale <natandovale496@gmail.com>
2021-10-09 14:29:35 -0300

    Laco de repeticao while

---
+++ EstruturaDeRepeticao/While/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 10)
+            {
+                WriteLine($"num => {i}");
+                i++;
+            }
+        }
+    }
+}
