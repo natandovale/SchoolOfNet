9850e24f064eef9c41be899e2348ea44dbd35e82
natandovale <natandovale496@gmail.com>
2021-10-09 10:25:04 -0300

    Filas

---
+++ EstruturasDeDados/Filas/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue fila = new Queue();
+            fila.Enqueue("Gilson");
+            fila.Enqueue("Amanda");
+            fila.Enqueue("Tio Patinhas");
+            fila.Enqueue("Tony Stark");
+
+            WriteLine($"A fila tem {fila.Count} itens.");
+
+            WriteLine($"O item {fila.Dequeue()} foi retirado.");
+
+            ReadKey();
+        }
+    }
+}
