95a45016cfe8fd686e0652ef040c311cea8b0ecd
natandovale <natandovale496@gmail.com>
2021-10-09 09:56:11 -0300

    Arrays basico

---
+++ EstruturasDeDados/Array/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[3];
+            x[0] = 5;
+            x[1] = 10;
+            x[2] = 15;
+
+            string[] nomes = new string[2];
+            nomes[0] = "Gilson";
+            nomes[1] = "Tio Patinhas";
+
+            string[] diasSemanas = new string[] { "Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab" };
+
+            WriteLine(x[0]);
+            WriteLine(nomes[1]);
+            WriteLine(diasSemanas[3]);
+            ReadKey();
+        }
+    }
+}
