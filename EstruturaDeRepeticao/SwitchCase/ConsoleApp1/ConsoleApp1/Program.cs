4dc3871481e6d3800c8df38ad0b6d2d7eefa823b
natandovale <natandovale496@gmail.com>
2021-10-09 14:19:04 -0300

    Estrutura de switch

---
+++ EstruturaDeRepeticao/SwitchCase/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Informe o saldo do cartão/banco.");
+            var eSaldo = double.Parse(ReadLine());
+
+            Write("Informe o valor da compra.");
+            var eValor = double.Parse(ReadLine());
+            
+            Write("Informe o tipo de pagamento (C para credito e D para debito.");
+            var eTipo = char.Parse(ReadLine());
+
+            (double saldo, double compra, char tipo) pagamento = (eSaldo, eValor, eTipo);
+
+            switch (pagamento.tipo)
+            {
+                case 'C' when pagamento.compra > pagamento.saldo:
+                    WriteLine("Compra com cartão de crédito não aprovado!");
+                    break;
+                case 'D' when pagamento.compra > pagamento.saldo:
+                    WriteLine("Compra com cartão de débito não aprovado!");
+                    break;
+                default:
+                    WriteLine("Compra aprovada");
+                    break;
+            }
+        }
+    }
+}
