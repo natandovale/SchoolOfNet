f332c100158c6166484dfe19dc7b4eb6fb2ca8bb
natandovale <natandovale496@gmail.com>
2021-10-09 15:38:10 -0300

    Criando conexao SqlConnection

---
+++ ADO.NET/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connString = "Server=DESKTOP-K7BEBRC\\SQLEXPRESS;Database=CSharpAdoNet;User Id=sa;Password=20101914nm";
+            SqlConnection conn = new SqlConnection(connString);
+
+            WriteLine(conn);
+        }
+    }
+}
